c71ed17826e45826935a1efeb1d6aa831de67af4
hudis flax <yf56229@gmail.com>
2022-10-24 16:36:50 +0300

    added none in enums should we take off?

--- Models/Enum.cs
+++ Models/Enum.cs
@@ -6,7 +6,7 @@ namespace theFoodCampus.Models
         public enum Kashrut { Dairy, Pareve, Meaty }
         public enum Difficulty { Easy, Intermediate, Difficult }
         public enum PrepTime { Quick, Medium, Long }
-        public enum Holiday { Rosh_Hashana, Yom_Kippur, Succot, Shmini_Atzeret, Hannuka, Tubishvat, Purim, Pesach, Shavouot }
+        public enum Holiday { RoshHashana, Yom_Kippur, Succot, Shmini_Atzeret, Hannuka, Tubishvat, Purim, Pesach, Shavouot}
         public enum Event { Exams, Vacation, Beginning_Of_Semester, End_Of_Semester, Project, Occasion }
         public enum Weather { Hot, Regular, Cold }
         public enum Category { Soup, Salad, Desert, CakeAndCookies, Breads, Fish, Chicken, Meat, Quiche, Pasta, }
